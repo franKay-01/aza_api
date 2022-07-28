fb7b9620bfb41edbf1f51fbcad0be41ed75e6b4e
franKay-01 <fkay0450@gmail.com>
2022-07-28 11:38:56 +0000

    adding validation to transaction model

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,2 +1,4 @@
 class Transaction < ApplicationRecord
+  validates :amount_currency, presence: {message: "Please select currency"}
+  validates :amount, :numericality => {:greater_than_equal => 0, message: "should be greater than 0"}
 end
